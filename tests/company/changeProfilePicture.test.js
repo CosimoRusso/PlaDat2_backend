32ceee9f27002521e7d2d7830d626e4504605500
Cosimo Russo <cosimo.russo123@gmail.com>
2021-01-01 21:27:27 +0100

    Add more tests for change profile picture and fix broken one

--- tests/company/changeProfilePicture.test.js
+++ tests/company/changeProfilePicture.test.js
@@ -1,18 +1,13 @@
-const r2 = require("r2");
 const FormData = require('form-data');
-const { update } = require("../../api/companies/company.controller");
 const {  Company } = require('../../models').models;
 const Sequelize = require('../../models/db');
 const signJWT = require("../../utils/signJWT");
-const { hash, compare } =require("../../utils/password");
 const cleanDatabase = require('../../utils/cleanDatabase.util');
 const fs = require("fs");
-const http = require("http");
-const md5File = require("md5-file");
 const fetch = require('node-fetch');
 const path = require("path");
-const {pipeline} = require("stream");
-const {promisify} = require("util");
+const md5 = require("md5");
+const { imageUpload } = require('../../api/companies/company.controller');
 
 const noop = () => {};
 const sequelize = new Sequelize().getInstance();
@@ -27,7 +22,29 @@ beforeAll(async () => {
 
 afterAll(cleanDatabase.bind(null, o, sequelize));
 
+test("No file uploaded in API", async () => {
+  const { company } = o;
+  const ctx = { user: company };
+  try{
+    await imageUpload(ctx, noop);
+  }catch(e){
+    expect(e.status).toBe(400);
+    expect(e.message).toBe("No image in the upload");
+  }
+});
+
 test("Company can change its profile picture", async () => {
+  const { company } = o;
+  const newProfilePicture = "https://myprofilepicture.com/hello.png";
+  const ctx = { user: company, file: { location: newProfilePicture } };
+  await imageUpload(ctx);
+  expect(ctx.status).toBe(200);
+  expect(ctx.body.picture).toBe(newProfilePicture);
+  await company.reload();
+  expect(company.picture).toBe(newProfilePicture);
+})
+
+test("Company can change its profile picture - API version", async () => {
   const { company } = o;
   const jwt = signJWT({id: company.id, userType: "company"});
   const url = "http://localhost:3000/api/v1/company/imageUpload";
@@ -44,13 +61,20 @@ test("Company can change its profile picture", async () => {
   expect(picture).toBeDefined();
   expect(picture).toBe(company.picture);
 
-  const streamPipeline = promisify(pipeline);
   const response = await fetch(picture);
   expect(response.ok).toBe(true);
-  const tmpPath = path.join(__dirname, "tmp.jpeg");
-  await streamPipeline(response.body, fs.createWriteStream(tmpPath));
-  const hash1 = await md5File(tmpPath);
-  const hash2 = await md5File(kittenPath);
+  const remoteKitten = await streamToString(response.body);
+  const localKitten = await fs.readFileSync(kittenPath, 'utf-8');
+  const hash1 = md5(remoteKitten);
+  const hash2 = md5(localKitten);
   expect(hash1).toBe(hash2);
-  fs.unlinkSync(tmpPath);
 });
+
+function streamToString (stream) {
+  const chunks = []
+  return new Promise((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk))
+    stream.on('error', reject)
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
+  })
+}
